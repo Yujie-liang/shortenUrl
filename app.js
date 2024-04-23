ca1e65eae848c0b17eddf258be077c7eefb4bb91
Yujie <b05613029@g.ntu.edu.tw>
2024-04-23 23:03:19 +0800

    build express framework and views

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 
+app.engine('.hbs', engine({ extname: 'hbs' }))
+app.set('view engine', '.hbs');
+app.set('views', './views');
+
+app.use(express.static('public'))
+// 為了解析 application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-  res.send('Hello!')
+  res.render('index')
 })
 
+app.post('/submit', (req, res) => {
+  const longUrl = req.body.url;
+  console.log(longUrl)
+  //const shortUrl = 
+  res.render('shortUrl', { url: req.body.url })//, { shortUrl })
+})
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`)
 })

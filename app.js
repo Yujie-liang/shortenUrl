7dba024875aeccf864275e64eae79a2021a6abb8
Yujie <b05613029@g.ntu.edu.tw>
2024-04-26 00:23:47 +0800

    build root and /submit route with generateRandomCode() function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
+let urlDatabase = {}
 
 app.engine('.hbs', engine({ extname: 'hbs' }))
 app.set('view engine', '.hbs');
@@ -17,10 +18,31 @@ app.get('/', (req, res) => {
 
 app.post('/submit', (req, res) => {
   const longUrl = req.body.url;
+  let code = ''
   console.log(longUrl)
-  //const shortUrl = 
-  res.render('shortUrl', { url: req.body.url })//, { shortUrl })
+  if (urlDatabase[longUrl] !== undefined) {
+    code = urlDatabase[longUrl]
+    console.log(!urlDatabase[longUrl])
+  } else {
+    code = generateRandomCode()
+    console.log(code)
+    urlDatabase[longUrl] = code
+  }
+  const shortUrl = `http://localhost:3000/${code}`
+  console.log(shortUrl)
+  res.render('shortUrl', { url: req.body.url, shortUrl })
 })
+
+function generateRandomCode() {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let result = '';
+  for (let i = 0; i < 5; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+
+  return result;
+}
+
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`)
 })

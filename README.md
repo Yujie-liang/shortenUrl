d35f16812661046921a984ee8b7de93cc2d13be8
Yujie <b05613029@g.ntu.edu.tw>
2024-04-27 15:13:38 +0800

    add copy url function and edit README.md

--- README.md
+++ README.md
@@ -8,6 +8,11 @@
 - 若使用者沒有輸入內容，就按下了送出鈕，需要防止表單送出並提示使用者
 - 使用者可以一鍵複製生成的短網址
 - 使用者可以透過按鈕返回主畫面，輸入其他網址
+- 使用者可以在生成短網址的畫面看到原始網址
+
+## 成品
+![home page](https://i.imgur.com/rIQ8E88.png)
+![shortUrl page](https://i.imgur.com/UptuwoJ.png)
 
 ## 開發環境
 ```
@@ -17,7 +22,7 @@ express
 ## 專案安裝
 1. 將此專案clone到本地
   ```
-  https://github.com/Yujie-liang/restaurant_list
+  https://github.com/Yujie-liang/shortenUrl.git
   ```
 2. 在本地的專案資料夾內安裝套件
   ```
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,27 @@
-document.querySelector('#search-form').addEventListener('submit', function (event) {
-  const inputURL = document.querySelector('#link-input').value;
-  if (inputURL.trim() === '') {
-    event.preventDefault();  // 阻止表單提交
-    alert('Please enter a URL to shorten.');
-  }
-});
+const submitButton = document.querySelector('#search-form')
+if (submitButton) {
+  submitButton.addEventListener('submit', function (event) {
+    const inputURL = document.querySelector('#link-input').value;
+    if (inputURL.trim() === '') {
+      event.preventDefault();  // 阻止表單提交
+      alert('Please enter a URL to shorten.');
+    }
+  });
+}
+
+const copyButton = document.querySelector('#copyButton')
+if (copyButton) {
+  copyButton.addEventListener('click', function (event) {
+    const url = document.querySelector('#shortUrl').href;  // 獲取shortURL
+    if (url) {
+      console.log(url)
+    } else {
+      console.log('nothing')
+    }
+    navigator.clipboard.writeText(url).then(function () {
+      alert('URL copied!');
+    }).catch(function (error) {
+      alert('Copy failed');
+    });
+  })
+}
